40fcc328f502980e795903c233a20993dd9c4c15
unknown <C:\The Bat!\Data\Mail>
2021-12-27 20:26:45 +0300

    Create Array func

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateRandomArray(int leftBound, int rightBound, int arrayLength)
+{
+    Random rand = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = rand.Next(leftBound, rightBound + 1);
+    }
+    return array;
+}
+
